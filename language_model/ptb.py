1) fix bug: add the final softmax layer weight to update function 2) fix bug: shift x in decoder 3) increment: add f_test in encoder 4) increment: rewrite output info, add weights info, i.e., shape

language_model/ptb.py
@@ -22,7 +22,7 @@ def init_params():
     params['test_period'] = 1
     params['exp_time'] = str(datetime.now().strftime('%Y%m%d-%H%M'))
     params['emb_dropout'] = 0.0
-    params['lr'] = 2e-3
+    params['lr'] = 2e-4
     
     # paths
     params['save_dir'] = '../results/' + params['exp_time'] + '/'
@@ -56,19 +56,26 @@ def train(params):
                          emb_dropout=params['emb_dropout'],
                          lr=params['lr'],
                          )
+    
 
     f_train = model.get_f_train()
     f_test = model.get_f_test()
 
     if not os.path.exists(params['save_dir']):
         os.mkdir(params['save_dir'])
+
     config_info = misc.configuration2str(params)
+    weights_info = misc.weightsinfo2str(model.get_params())
 
     df = open(params['details_log_path'], 'w')
     lf = open(params['log_path'], 'w')
+
     df.write(config_info + '\n')
     lf.write(config_info + '\n')
     print(config_info)
+    df.write(weights_info)
+    lf.write(weights_info)
+    print(weights_info)
     
     num_batches_train = params['num_samples_train'] / params['batch_size']
     num_batches_valid = params['num_samples_valid'] / params['batch_size']
@@ -76,48 +83,61 @@ def train(params):
     
     cur = time.time()
     for epoch in range(params['num_epochs']):
-        epoch_info = ' [*] epoch %d \n' % epoch
-        lf.write(epoch_info)
-        df.write(epoch_info)
+        epoch_info = ' [*] epoch %d ' % epoch
+        lf.write(epoch_info + '\n')
+        df.write(epoch_info + '\n')
         print(epoch_info)
     
-        outs = []
+        out_all, llh_all, nw_all = [], [], []
         for batch in range(num_batches_train):
             x, p, a = it_train.next()
             x, m, p, a = misc.prepare_ptb([x, p, a])
             out = f_train(x, m, p, a)
-            outs.append(out)
-            #print('\t[-] train: ' + str(out) + '\n')
+            out_all.append(out)
+            llh_all.append(out[0] * params['batch_size'])
+            nw_all.append(np.sum(p))
             df.write('\t[-] train: ' + str(out) + '')
-        lf.write('\t[-] train: ' + str(np.mean(outs, axis=0)) + '')
-        print('\t[-] train: ' + str(np.mean(outs, axis=0)) + '')
-        print('\t[-] time:' + str(time.time() - cur) + '\n')
-        
-        outs = []
+        ppl = np.exp(np.sum(llh_all)/ np.sum(nw_all))
+        train_info = '\t[-] train: ' + str(np.mean(out_all, axis=0)) + ' ' + str(np.sum(llh_all)) + ' ' + str(np.sum(nw_all)) + ' '+ str(ppl)
+        print('\t[-] time:' + str(time.time() - cur))
+        df.write(train_info + '\n')
+        lf.write(train_info + '\n')
+        print(train_info)
+     
+        out_all, llh_all, nw_all = [], [], []
         for batch in range(num_batches_valid):
             x, p, a = it_valid.next()
             x, m, p, a = misc.prepare_ptb([x, p, a])
             out = f_test(x, m, p, a)
-            outs.append(out)
-            #print('\t[-] valid: ' + str(out) + '\n')
+            out_all.append(out)
+            llh_all.append(out[0] * params['batch_size'])
+            nw_all.append(np.sum(p))
             df.write('\t[-] valid: ' + str(out) + '')
-        lf.write('\t[-] valid: ' + str(np.mean(outs, axis=0)) + '')
-        print('\t[-] valid: ' + str(np.mean(outs, axis=0)) + '\n')
-        print('\t[-] time:' + str(time.time() - cur) + '\n')
-        
-        outs = []
+        ppl = np.exp(np.sum(llh_all)/ np.sum(nw_all))
+        valid_info = '\t[-] valid: ' + str(np.mean(out_all, axis=0)) + ' ' + str(np.sum(llh_all)) + ' ' + str(np.sum(nw_all)) + ' '+ str(ppl)
+        print('\t[-] time:' + str(time.time() - cur))
+        df.write(valid_info)
+        lf.write(valid_info)
+        print(valid_info)
+
+        out_all, llh_all, nw_all = [], [], []
         for batch in range(num_batches_test):
             x, p, a = it_test.next()
             x, m, p, a = misc.prepare_ptb([x, p, a])
             out = f_test(x, m, p, a)
-            outs.append(out)
-            #print('\t[-] test: ' + str(out) + '\n')
+            out_all.append(out)
+            llh_all.append(out[0] * params['batch_size'])
+            nw_all.append(np.sum(p))
             df.write('\t[-] test: ' + str(out) + '')
-        lf.write('\t[-] test: ' + str(np.mean(outs, axis=0)) + '')
-        print('\t[-] test: ' + str(np.mean(outs, axis=0)) + '\n')
-        print('\t[-] time:' + str(time.time() - cur) + '\n')
-       
-        #model.save_weights(params['save_weights_path'])
+        ppl = np.exp(np.sum(llh_all)/ np.sum(nw_all))
+        test_info = '\t[-] test: ' + str(np.mean(outs, axis=0)) + ' ' + str(np.sum(llh_all)) + ' ' + str(np.sum(nw_all)) + ' '+ str(ppl)
+        print('\t[-] time:' + str(time.time() - cur))
+        df.write(test_info)
+        lf.write(test_info)
+        print(test_info)
+        
+        # todo
+        # model.save_weights(params['save_weights_path'])
 
 
 if __name__ == '__main__':

models/stack_lstm_lm.py
@@ -33,28 +33,38 @@ class StackLSTMLM(object):
         w_emb = w_emb.astype(theano.config.floatX)
         
         # single example encoder
-        self.l_x = InputLayer((None, None)) # flattened input
-        self.l_m = InputLayer((None, None)) # mask
-        self.l_p = InputLayer((None, None)) # if predict
-        self.l_a = InputLayer((None, None)) # idx for ancestor in stack
-        self.l_emb = EmbeddingLayer(self.l_x, dict_size, dim_emb, W=w_emb)
-        self.l_emb = DropoutLayer(self.l_emb, emb_dropout)
+        self.l_x = InputLayer((None, None), name='l_x') # flattened input
+        self.l_m = InputLayer((None, None), name='l_m') # mask
+        self.l_p = InputLayer((None, None), name='l_p') # if predict
+        self.l_a = InputLayer((None, None), name='l_a') # idx for ancestor in stack
+        self.l_emb = EmbeddingLayer(self.l_x, dict_size, dim_emb, W=w_emb, name='l_emb')
+        self.l_emb = DropoutLayer(self.l_emb, emb_dropout, name='l_emb_drop')
         self.l_dec = StackLSTMDecoder([self.l_emb, self.l_m, self.l_p, self.l_a,],
                                        num_units = self.num_units,
                                        only_return_final=False,
+                                       name='l_dec',
                                        )
-        self.l_prd = ReshapeLayer(self.l_dec, [-1, num_units])
-        self.l_prd = DenseLayer(self.l_prd, dict_size, 
-                                nonlinearity=nonlinearities.softmax)
+        self.l_dec = ReshapeLayer(self.l_dec, [-1, num_units], name='l_reshape')
+        self.l_prd_x = DenseLayer(self.l_dec, dict_size, 
+                                nonlinearity=nonlinearities.softmax, name='l_prd_x')
+        self.l_prd_p = DenseLayer(self.l_dec, 2, 
+                                nonlinearity=nonlinearities.softmax, name='l_prd_p')
 
 
-    def _forward(self, inputs):
+    def _forward(self, inputs, deterministic=False):
         x, m, p, a = inputs
-        h = get_output(self.l_prd, {self.l_x: x,
+        e = get_output(self.l_emb, {self.l_x: x})
+
+        # shift the embedding
+        e_shift = T.zeros_like(e)
+        e_shift = T.set_subtensor(e_shift[:,1:,:], e[:,:-1,:])
+
+        px, pp = get_output([self.l_prd_x, self.l_prd_p], {self.l_emb: e_shift,
                                     self.l_m: m,
                                     self.l_p: p,
-                                    self.l_a: a,})
-        return h
+                                    self.l_a: a,},
+                                    deterministic=deterministic)
+        return px, pp
     
     def get_f_train(self,):
         x = T.lmatrix()
@@ -63,22 +73,29 @@ class StackLSTMLM(object):
         a = T.lmatrix()
         
         inputs = [x, m, p, a]
-        pred = self._forward(inputs)
+        px, pp = self._forward(inputs, False)
         batch_size, seq_len = x.shape
         
-        l = objectives.categorical_crossentropy(pred, x.flatten())
-        l = l.reshape([batch_size, seq_len])
-        l = (l * p).sum(axis=1)
-        ppl = T.exp(T.sum(l) / T.sum(p))
-        l = l.mean()
+        lx = objectives.categorical_crossentropy(px, x.flatten())
+        lx = lx.reshape([batch_size, seq_len])
+        lx = (lx * p).sum(axis=1)
+        lx = lx.mean()
+        
+        # although it is wrong to cal ppl this way, it remains for batch testing.
+        ppl = T.exp(T.sum(lx) / T.sum(p))
+
+        lp = objectives.categorical_crossentropy(pp, p.flatten())
+        lp = lp.reshape([batch_size, seq_len])
+        lp = (lp * m).sum(axis=1)
+        lp = lp.mean()
 
         params = self.get_params()
-        grads = theano.grad(l, params)
+        grads = theano.grad(lx+lp, params)
         grads = [T.clip(g, -10, 10) for g in grads]
         grads = updates.total_norm_constraint(grads, max_norm=20)
         update = updates.adam(grads, params, self.lr)
         
-        f_train = theano.function(inputs, [l, ppl], updates=update)
+        f_train = theano.function(inputs, [lx, lp, ppl], updates=update)
         return f_train
         
     def get_f_test(self,):
@@ -86,18 +103,24 @@ class StackLSTMLM(object):
         m = T.matrix()
         p = T.lmatrix()
         a = T.lmatrix()
-        
+         
         inputs = [x, m, p, a]
-        pred = self._forward(inputs)
+        px, pp = self._forward(inputs, True)
         batch_size, seq_len = x.shape
         
-        l = objectives.categorical_crossentropy(pred, x.flatten())
-        l = l.reshape([batch_size, seq_len])
-        l = (l * p).sum(axis=1)
-        ppl = T.exp(T.sum(l) / T.sum(p))
-        l = l.mean()
-        
-        f_test = theano.function(inputs, [l,ppl])
+        lx = objectives.categorical_crossentropy(px, x.flatten())
+        lx = lx.reshape([batch_size, seq_len])
+        lx = (lx * p).sum(axis=1)
+        lx = lx.mean()
+
+        ppl = T.exp(T.sum(lx) / T.sum(p))
+
+        lp = objectives.categorical_crossentropy(pp, p.flatten())
+        lp = lp.reshape([batch_size, seq_len])
+        lp = (lp * m).sum(axis=1)
+        lp = lp.mean()
+
+        f_test = theano.function(inputs, [lx, lp ,ppl])
         return f_test
        
     
@@ -112,5 +135,5 @@ class StackLSTMLM(object):
 
 
     def get_params(self,):
-        params = get_all_params(self.l_dec)
+        params = get_all_params([self.l_prd_x, self.l_prd_p])
         return params

models/stack_lstm_sim.py
@@ -46,57 +46,61 @@ class StackLSTMSim(object):
         w_emb = w_emb.astype(theano.config.floatX)
         
         # single example encoder
-        self.l_x = InputLayer((None, None))
-        self.l_m = InputLayer((None, None))
-        self.l_a = InputLayer((None, None))
-        self.l_p = InputLayer((None, None, 2))
-        self.l_emb = EmbeddingLayer(self.l_x, dict_size, dim_emb, W=w_emb)
-        self.l_emb = DropoutLayer(self.l_emb, emb_dropout)
-        self.l_xc = LinearLayer(self.l_emb, num_units_stack, W=init.HeUniform())
-        self.l_xh = LinearLayer(self.l_emb, num_units_stack, W=init.HeUniform())
-        self.l_enc = StackLSTMEncoder([self.l_xc, self.l_xh, self.l_m, self.l_a,
-                                       self.l_p], 
+        self.l_x = InputLayer((None, None), name='l_x')     # flattened x
+        self.l_m = InputLayer((None, None), name='l_m')     # mask
+        self.l_a = InputLayer((None, None), name='l_a')     # action \in (reduce, shift)
+        self.l_p = InputLayer((None, None, 2), name='l_p')  # the indices of two elements in stack
+        self.l_emb = EmbeddingLayer(self.l_x, dict_size, dim_emb, W=w_emb, name='l_emb')
+        self.l_emb = DropoutLayer(self.l_emb, emb_dropout, name='l_emb_drop')
+        self.l_xc = LinearLayer(self.l_emb, num_units_stack, W=init.HeUniform(), name='l_xc')
+        self.l_xh = LinearLayer(self.l_emb, num_units_stack, W=init.HeUniform(), name='l_xh')
+        self.l_enc = StackLSTMEncoder([self.l_xc, self.l_xh, self.l_m, self.l_a, self.l_p], 
                                        num_units_stack = self.num_units_stack,
-                                       num_units_track = self.num_units_track
+                                       num_units_track = self.num_units_track,
+                                       name='l_enc',
                                        )
 
-        self.l_in_a = InputLayer([None, num_units_track])
-        self.l_clf_a = DropoutLayer(self.l_in_a, clf_dropout)
+        self.l_in_a = InputLayer([None, num_units_track], name='l_a_aux')
+        self.l_clf_a = DropoutLayer(self.l_in_a, clf_dropout, name='l_a_aux_drop')
         self.l_clf_a = DenseLayer(self.l_clf_a, 2, W=init.Uniform(5e-3), 
-                                  nonlinearity=nonlinearities.softmax)
+                                  nonlinearity=nonlinearities.softmax,
+                                  name='l_clf_a')
 
-        self.l_in_t = InputLayer([None, 4*num_units_stack])
-        self.l_clf_t = DropoutLayer(self.l_in_t, clf_dropout)
+        self.l_in_t = InputLayer([None, 4*num_units_stack], name='l_t_aux')
+        self.l_clf_t = DropoutLayer(self.l_in_t, clf_dropout, name='l_t_aux_drop')
         self.l_clf_t = DenseLayer(self.l_clf_t, num_classes, W=init.Uniform(5e-3),
-                                  nonlinearity=nonlinearities.softmax)
+                                  nonlinearity=nonlinearities.softmax,
+                                  name='l_clf_t')
 
 
-    def _forward(self, inputs):
+    def _forward(self, inputs, deterministic=False):
         x0, m0, a0, p0, x1, m1, a1, p1 = inputs
 
-        xc0, xh0 = get_output([self.l_xc, self.l_xh], {self.l_x: x0})
-        xc1, xh1 = get_output([self.l_xc, self.l_xh], {self.l_x: x1})
+        xc0, xh0 = get_output([self.l_xc, self.l_xh], {self.l_x: x0}, deterministic=deterministic)
+        xc1, xh1 = get_output([self.l_xc, self.l_xh], {self.l_x: x1}, deterministic=deterministic)
 
         enc0, e0 = get_output(self.l_enc, {self.l_xc: xc0,
                          self.l_xh: xh0,
                          self.l_m: m0,
                          self.l_a: a0,
-                         self.l_p: p0,})
+                         self.l_p: p0,}
+                         deterministic=deterministic)
 
         e0 = e0.reshape([-1, self.num_units_track])
-        pa0 = get_output(self.l_clf_a, {self.l_in_a: e0})
+        pa0 = get_output(self.l_clf_a, {self.l_in_a: e0}, deterministic=deterministic)
 
         enc1, e1 = get_output(self.l_enc, {self.l_xc: xc1,
                          self.l_xh: xh1,
                          self.l_m: m1,
                          self.l_a: a1,
-                         self.l_p: p1,})
+                         self.l_p: p1,},
+                         deterministic=deterministic)
 
         e1 = e1.reshape([-1, self.num_units_track])
-        pa1 = get_output(self.l_clf_a, {self.l_in_a: e1})
+        pa1 = get_output(self.l_clf_a, {self.l_in_a: e1}, deterministic=deterministic)
 
         clf_t_in = T.concatenate([enc0, enc1, enc0-enc1, enc0*enc1], axis=1)
-        t = get_output(self.l_clf_t,  {self.l_in_t: clf_t_in})
+        t = get_output(self.l_clf_t,  {self.l_in_t: clf_t_in}, deterministic=deterministic)
        
         # return flattened pa0 and pa1
         return pa0, pa1, t
@@ -110,7 +114,7 @@ class StackLSTMSim(object):
         y = T.lvector()
         
         inputs = [x0, m0, a0, p0, x1, m1, a1, p1]
-        pa0, pa1, t = self._forward(inputs)
+        pa0, pa1, t = self._forward(inputs, False)
 
         lt = categorical_crossentropy(t, y)
         lpa0 = categorical_crossentropy(pa0, a0.flatten())
@@ -132,7 +136,35 @@ class StackLSTMSim(object):
         f_train = theano.function(inputs, l, updates=update)
 
         return f_train
+     
+    def get_f_test(self,):
+        x0, x1 = T.lmatrix(), T.lmatrix()
+        m0, m1 = T.matrix(), T.matrix()
+        a0, a1 = T.lmatrix(), T.lmatrix()
+        p0, p1 = T.ltensor3(), T.ltensor3()
+        y = T.lvector()
         
+        inputs = [x0, m0, a0, p0, x1, m1, a1, p1]
+        pa0, pa1, t = self._forward(inputs, False)
+
+        lt = categorical_crossentropy(t, y)
+        lpa0 = categorical_crossentropy(pa0, a0.flatten())
+        lpa1 = categorical_crossentropy(pa1, a1.flatten())
+
+        lpa0 = lpa0.reshape(x0.shape)
+        lpa1 = lpa1.reshape(x1.shape)
+
+        lpa0 = (lpa0 * m0).sum(axis=1)
+        lpa1 = (lpa1 * m1).sum(axis=1)
+
+        l = (self.alpha*(lpa0 + lpa1) + lt).mean()
+        l += self._l2_regularization()
+        
+        inputs += [y]
+        f_test = theano.function(inputs, l)
+
+        return f_test
+
     
     def _l2_regularization(self,):
         params = self.get_params()
@@ -146,6 +178,8 @@ class StackLSTMSim(object):
 
     def get_params(self,):
         params = []
+        params += self.l_xh.get_params()
+        params += self.l_xc.get_params()
         params += self.l_enc.get_params()
         params += self.l_clf_a.get_params()
         params += self.l_clf_t.get_params()

relatedness/snli.py
@@ -69,18 +69,24 @@ def train(params):
                          )
 
     f_train = model.get_f_train()
-    # debug
-    f_test = f_train
+    f_test = model.get_f_test()
 
     if not os.path.exists(params['save_dir']):
         os.mkdir(params['save_dir'])
+
     config_info = misc.configuration2str(params)
+    weights_info = misc.weight_info2str(model.get_params())
 
     df = open(params['details_log_path'], 'w')
     lf = open(params['log_path'], 'w')
+
     df.write(config_info + '\n')
     lf.write(config_info + '\n')
     print(config_info)
+
+    df.write(weights_info + '\n')
+    lf.write(config_info + '\n')
+    print(weights_info)
     
     num_batches_train = params['num_samples_train'] / params['batch_size']
     num_batches_valid = params['num_samples_valid'] / params['batch_size']
@@ -88,22 +94,36 @@ def train(params):
 
     for epoch in range(params['num_epochs']):
         epoch_info = ' [*] epoch %d \n' % epoch
-        lf.write(epoch_info)
-        df.write(epoch_info)
+        lf.write(epoch_info + '\n')
+        df.write(epoch_info + '\n')
         print(epoch_info)
     
-        outs = []
+        out_all = []
         for batch in range(num_batches_train):
             x0, x1, a0, a1, p0, p1, y = it_train.next()
             x0, m0, a0, p0 = misc.prepare_snli([x0, a0, p0])
             x1, m1, a1, p1 = misc.prepare_snli([x1, a1, p1])
             out = f_train(x0, m0, a0, p0, x1, m1, a1, p1, y)
-            outs.append(out)
+            out_all.append(out)
             #print('\t[-] train: ' + str(out) + '\n')
             df.write('\t[-] train: ' + str(out) + '\n')
-        lf.write('\t[-] train: ' + str(np.mean(outs, axis=0)) + '\n')
-        print('\t[-] train: ' + str(np.mean(outs, axis=0)) + '\n')
-        
+        train_info = '\t[-] train: ' + str(np.mean(out_all, axis=0))
+        lf.write(train_info + '\n')
+        print(train_info)
+     
+        out_all = []
+        for batch in range(num_batches_valid):
+            x0, x1, a0, a1, p0, p1, y = it_test.next()
+            x0, m0, a0, p0 = misc.prepare_snli([x0, a0, p0])
+            x1, m1, a1, p1 = misc.prepare_snli([x1, a1, p1])
+            out = f_test(x0, m0, a0, p0, x1, m1, a1, p1, y)
+            out_all.append(out)
+            #print('\t[-] train: ' + str(out) + '\n')
+            df.write('\t[-] train: ' + str(out) + '\n')
+        train_info = '\t[-] train: ' + str(np.mean(out_all, axis=0))
+        lf.write(train_info + '\n')
+        print(train_info)    
+
         model.save_weights(params['save_weights_path'])
 
 